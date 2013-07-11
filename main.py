a595443e58f92f9381220a0874bb5320c5dbe4b5
Michael McClellan <mcclellan.m@gmail.com>
2013-07-11 16:02:06 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Module docstring.
+"""
+
+import sys
+
+def main(argv=None):
+    # application code here, like:
+    return 0
+    
+    
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
